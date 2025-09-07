4c8ca37390100f937b151d11f016786ebd60b832
Kanoa Parker <kanparker@hmc.edu>
2025-09-06 17:01:33 -0700

    case statement based seven module

--- fpga/src/e155_lab1/impl_1/e155_lab1_impl_1_map.tcl
+++ fpga/src/e155_lab1/impl_1/e155_lab1_impl_1_map.tcl
@@ -4,16 +4,16 @@
 set ret 0
 if {[catch {
 
-if {![file exists {C:/Users/kanparker/Downloads/E155-lab1/fpga/src/e155_lab1/impl_1}]} {
-  file mkdir {C:/Users/kanparker/Downloads/E155-lab1/fpga/src/e155_lab1/impl_1}
+if {![file exists {C:/Users/kanparker/Downloads/e155-lab1/E155-lab1/fpga/src/e155_lab1/impl_1}]} {
+  file mkdir {C:/Users/kanparker/Downloads/e155-lab1/E155-lab1/fpga/src/e155_lab1/impl_1}
 }
-cd {C:/Users/kanparker/Downloads/E155-lab1/fpga/src/e155_lab1/impl_1}
+cd {C:/Users/kanparker/Downloads/e155-lab1/E155-lab1/fpga/src/e155_lab1/impl_1}
 
-sys_set_attribute -gui on -msg {C:/Users/kanparker/Downloads/E155-lab1/fpga/src/e155_lab1/promote.xml}
-msg_load {C:/Users/kanparker/Downloads/E155-lab1/fpga/src/e155_lab1/promote.xml}
+sys_set_attribute -gui on -msg {C:/Users/kanparker/Downloads/e155-lab1/E155-lab1/fpga/src/e155_lab1/promote.xml}
+msg_load {C:/Users/kanparker/Downloads/e155-lab1/E155-lab1/fpga/src/e155_lab1/promote.xml}
 des_set_project_udb -in {e155_lab1_impl_1_syn.udb} -out {e155_lab1_impl_1_map.udb} -milestone map -pm ice40tp
 des_set_reference_udb -clean
-map_set_option {pdc_file "C:/Users/kanparker/Downloads/E155-lab1/fpga/src/e155_lab1/e155_lab1.pdc"}
+map_set_option {pdc_file "C:/Users/kanparker/Downloads/e155-lab1/E155-lab1/fpga/src/e155_lab1/e155_lab1.pdc"}
 # map option
 map_set_option { report_symbol_cross_reference false report_signal_cross_reference false   ignore_constraint_errors false}
 map_run
--- fpga/src/e155_lab1/source/impl_1/seven_seg_display.sv
+++ fpga/src/e155_lab1/source/impl_1/seven_seg_display.sv
@@ -10,6 +10,7 @@ module seven_seg_display(
 	assign C = s[1];
 	assign D = s[0];
 	
+	/*
 	assign seg[6] = (~A&~C)&(B^D) | A&(~B&C&D | B&~C);
 	assign seg[5] = D&~(A^C) | B&(C&~D | A&~C&~D);
 	assign seg[4] = ~A&~B&C&~D | (A&B)&( ~C&~D | C);
@@ -17,6 +18,44 @@ module seven_seg_display(
 	assign seg[2] = (~A&~B&D) | (~A&B&~C) | (~A&B&C&D) | (A&~B&~C&D);
 	assign seg[1] = (~(A|B|C) & D) | (~A&~B&C) | (~A&B&C&D) | (A&B&~C);
 	assign seg[0] = ~(A|B|C|D) | (~A&~B&~C&D) | (~A&B&C&D);
+	*/
+	
+	always_comb
+		case(s[4:0])
+			/// 0
+			0000: seg[6:0] = 7'b0000001;
+			/// 1
+			0001: seg[6:0] = 7'b1001111;
+			/// 2
+			0010: seg[6:0] = 7'b0010010;
+			/// 3
+			0011:seg[6:0] = 7'b0000110;
+			/// 4
+			0100:seg[6:0] = 7'b1001100;
+			/// 5
+			0101:seg[6:0] = 7'b0100100;
+			/// 6
+			0110:seg[6:0] = 7'b0100000;
+			/// 7
+			0111:seg[6:0] = 7'b0001111;
+			/// 8
+			1000:seg[6:0] = 7'b0000000;
+			/// 9
+			1001:seg[6:0] = 7'b0001100;
+			/// A
+			1010:seg[6:0] = 7'b0001000;
+			/// B
+			1011:seg[6:0] = 7'b1100000;
+			/// C
+			1100:seg[6:0] = 7'b0110001;
+			/// D
+			1101:seg[6:0] = 7'b1000010;
+			/// E
+			1110:seg[6:0] = 7'b0110000;
+			/// F
+			1111:seg[6:0] = 7'b0111000;
+		endcase
+			
 	
 
 endmodule
